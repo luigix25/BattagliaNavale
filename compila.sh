608d795ad75c8d55addd603b1c07ed8c5b7d6cff
Luigi <luigix25@gmail.com>
2017-07-12 14:16:22 +0200

    Very Last

--- compila.sh
+++ compila.sh
@@ -5,7 +5,7 @@ rm *.o
 
 gcc -c client.c -o client.o -Wall
 gcc -c server.c -o server.o -Wall
-gcc -c library/library.c -o library.o
+gcc -c library/library.c -o library.o -Wall
 
 gcc client.o library.o -o client -Wall
 gcc server.o library.o -o server -Wall
